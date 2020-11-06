551195f4cdbeae07e1ae12cc3b9f6ebad6be1fab
Scott Lynch <62553406+slteksystemsllc@users.noreply.github.com>
2020-11-06 13:43:01 -0500

    Update prereq.sh

--- scripts/prereq.sh
+++ scripts/prereq.sh
@@ -60,7 +60,7 @@ then
   echo "Docker Compose is already installed"
 else
   echo "Installing Docker Compose"
-  sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 fi
 if grep -q 'vm.max_map_count' /etc/sysctl.conf
