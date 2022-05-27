3c2fac7c9823c6959018bb893b1fd7a585ce68f9
Scott Lynch <62553406+slteksystemsllc@users.noreply.github.com>
2022-05-27 20:06:33 +0300

    Update initialize.sh

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,60 +1,60 @@
 #!/bin/bash
 cd /opt/
 
-# Set hostname for sinlge node Elasticsearch
+# Set hostname for sinlge node opensearch
 hostnamectl set-hostname "es01. local"
 exec bash
 
-git clone https://github.com/slteksystemsllc/elasticsearch_siem.git
+git clone https://github.com/slteksystemsllc/opensearch_siem.git
 
-sudo chown 1000:1000 -R /opt/elasticsearch_siem/
+sudo chown 1000:1000 -R /opt/opensearch_siem/
 #######################################################
 # Elasticsearc Pre-Config
 
-mkdir -p /usr/share/elasticsearch/
-mkdir -p /usr/share/elasticsearch/cron/custom-cron/
-mkdir -p /usr/share/elasticsearch/data
+mkdir -p /opt/opensearch/
+mkdir -p /opt/opensearch/cron/custom-cron/
+mkdir -p /opt/opensearch/data
 
-sudo chown 1000:1000 -R /usr/share/elasticsearch/
+sudo chown 1000:1000 -R /opt/opensearch/
 
-cp -f /opt/elasticsearch_siem/docker-compose.yml.example /usr/share/elasticsearch/docker-compose.yml
-cp -f /opt/elasticsearch_siem/cron/custom-cron.example /usr/share/elasticsearch/cron/custom-cron/
+cp -f /opt/opensearch_siem/docker-compose.yml.example /opt/opensearch/docker-compose.yml
+cp -f /opt/opensearch_siem/cron/custom-cron.example /opt/opensearch/cron/custom-cron/
 
 #########################################################
 # Logstash Pre-Config 
 
-mkdir -p /usr/share/logstash/
-mkdir -p /usr/share/logstash/bin/
-mkdir -p /usr/share/logstash/config/
-mkdir -p /usr/share/logstash/configs/
-mkdir -p /usr/share/logstash/custom_patterns/
-mkdir -p /usr/share/logstash/dictionaries/
-mkdir -p /usr/share/logstash/persistent_data/
-mkdir -p /usr/share/logstash/pipeline/
-mkdir -p /usr/share/logstash/plugins/
-mkdir -p /usr/share/logstash/rules/
+mkdir -p /opt/logstash/
+mkdir -p /opt/logstash/bin/
+mkdir -p /opt/logstash/config/
+mkdir -p /opt/logstash/configs/
+mkdir -p /opt/logstash/custom_patterns/
+mkdir -p /opt/logstash/dictionaries/
+mkdir -p /opt/logstash/persistent_data/
+mkdir -p /opt/logstash/pipeline/
+mkdir -p /opt/logstash/plugins/
+mkdir -p /opt/logstash/rules/
 
-sudo chown 1000:1000 -R /usr/share/logstash/
+sudo chown 1000:1000 -R /opt/logstash/
 
-cp -r /opt/elasticsearch_siem/logstash/logstash_configs/* /usr/share/logstash/configs/
-cp -r /opt/elasticsearch_siem/logstash/dictionaries/* /usr/share/logstash/dictionaries/
-cp -f /opt/elasticsearch_siem/logstash/pipelines.yml.example /usr/share/logstash/config/pipelines.yml
+cp -r /opt/opensearch_siem/logstash/logstash_configs/* /opt/logstash/configs/
+cp -r /opt/opensearch_siem/logstash/dictionaries/* /opt/logstash/dictionaries/
+cp -f /opt/opensearch_siem/logstash/pipelines.yml.example /opt/logstash/config/pipelines.yml
 
 ############################################################
 # Elastalert Pre-Config
 
-mkdir -p /usr/share/elastalert/
-mkdir -p /usr/share/elastalert/config/
-mkdir -p /usr/share/elastalert/rules/
-mkdir -p /usr/share/elastalert/rule_templates/
-mkdir -p /usr/share/elastalert/elastalert-server/config/
+mkdir -p /opt/elastalert/
+mkdir -p /opt/elastalert/config/
+mkdir -p /opt/elastalert/rules/
+mkdir -p /opt/elastalert/rule_templates/
+mkdir -p /opt/elastalert/elastalert-server/config/
 
-sudo chown 1000:1000 -R /usr/share/elastalert/
+sudo chown 1000:1000 -R /opt/elastalert/
 
-cp -r /opt/elasticsearch_siem/elastalert/example_rules/* /usr/share/elastalert/rules/
-cp -f /opt/elasticsearch_siem/elastalert/config.yaml.example /usr/share/elastalert/config.yaml
-cp -f /opt/elasticsearch_siem/elastalert/config.json /usr/share/elastalert/elastalert-server/config/config.json
+cp -r /opt/opensearch_siem/elastalert/example_rules/* /opt/elastalert/rules/
+cp -f /opt/opensearch_siem/elastalert/config.yaml.example /opt/elastalert/config.yaml
+cp -f /opt/opensearch_siem/elastalert/config.json /opt/elastalert/elastalert-server/config/config.json
 
-cd /usr/share/elasticsearch/
+cd /opt/opensearch/
 
 docker-compose up --no-start
